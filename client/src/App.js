d1c99e9312f056e2c5515e1764169a385e6c5512
DavidBlumenshine <119887262+DavidBlumenshine@users.noreply.github.com>
2024-03-02 23:10:30 -0600

    fix isError return on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
     return <div>Loading...</div>;
   }
   if (isError) {
-    return <div>Error: (error.message)</div>
+    return <div>Error: {error.message}</div>
   }
   return (
     <>
