999df3e5156d33a137e3a82aa306cfabacefe09c
aidanho1188 <aaron23657@gmail.com>
2024-03-03 00:41:56 -0500

    push code

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require('mongoose')
+const {Schema} = mongoose
 
 const postSchema = new Schema({
-  title: { type: String, default: 'No Title' },
-  content: { type: String, default: '' },
-  date: { type: Date, default: Date.now }
-});
+  title: {type: String, default: 'No Title'},
+  content: {type: String, default: ''},
+})
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Post', postSchema)
